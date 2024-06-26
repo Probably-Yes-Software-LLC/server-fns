7874b371a30f2765647b9f769480595cce42665c
Conner Blair <conner@probablyyes.software>
2024-06-25 17:46:04 -0700

    save

--- core/src/out.rs
+++ core/src/out.rs
@@ -1,8 +1,9 @@
+use itertools::Itertools;
 use proc_macro2::{Span, TokenStream as TokenStream2};
 use quote::{format_ident, quote_spanned, ToTokens, TokenStreamExt};
 use syn::{
-    punctuated::Punctuated, spanned::Spanned, token::Comma, Block, FnArg, Generics, Ident, ItemFn,
-    ReturnType, TypeGenerics, WhereClause
+    parse_quote, parse_quote_spanned, punctuated::Punctuated, spanned::Spanned, token::Comma,
+    Block, Expr, FnArg, Generics, Ident, ItemFn, PatType, ReturnType, TypeGenerics, WhereClause
 };
 
 pub struct ServerFn {
@@ -36,7 +37,6 @@ pub struct InnerHandler {
 }
 
 mod server_fn {
-
     use super::*;
 
     impl ServerFn {
@@ -47,7 +47,26 @@ mod server_fn {
             let router_fn = format_ident!("{fn_ident}_router");
             let router_mod = format_ident!("__{router_fn}");
 
-            let router_fn = RouterFn::try_new(router_fn, &server_fn.sig.inputs)?;
+            let args_span = server_fn.sig.inputs.span();
+            let input_args = server_fn
+                .sig
+                .inputs
+                .iter()
+                .map(|arg| match arg {
+                    FnArg::Receiver(rec) => Err(syn::Error::new(
+                        rec.span(),
+                        "Reciever type 'self' is not supported in server functions."
+                    )),
+                    FnArg::Typed(typ) => Ok(typ)
+                })
+                .fold_ok(vec![], |args, next| {
+                    args.push(next);
+                    args
+                })?
+                .into_iter()
+                .enumerate();
+
+            let router_fn = RouterFn::try_new(args_span, router_fn, input_args)?;
             let stateful_handler = StatefulHandler::try_new()?;
             let inner_handler = InnerHandler::try_new(server_fn)?;
 
@@ -85,14 +104,74 @@ mod server_fn {
 }
 
 mod router_fn {
+
+    use syn::WherePredicate;
+
     use super::*;
 
     impl RouterFn {
-        pub fn try_new(
+        pub fn try_new<'a>(
+            span: Span,
             ident: Ident,
-            inputs: &Punctuated<FnArg, Comma>
+            inputs: impl IntoIterator<Item = (usize, &'a PatType)>
         ) -> Result<Self, syn::Error> {
-            let span = inputs.span();
+            // Assemble generics and arguments.
+            #[derive(Default)]
+            struct BuildArgs {
+                gens: Generics,
+                args: Punctuated<FnArg, Comma>,
+                state_args: Vec<Expr>
+            }
+
+            let state_attr = parse_quote!(#[state]);
+
+            let BuildArgs {
+                gens,
+                args,
+                state_args
+            } = inputs
+                .into_iter()
+                .fold(BuildArgs::default(), |mut build_args, (i, next)| {
+                    if next.attrs.contains(&state_attr) {
+                        let next_span = next.span();
+                        let next_type = &next.ty;
+                        let state_type = format_ident!("State{i}");
+                        let state_arg = format_ident!("state{i}");
+
+                        build_args
+                            .gens
+                            .lt_token
+                            .get_or_insert_with(|| parse_quote_spanned! { next_span => < });
+                        build_args
+                            .gens
+                            .gt_token
+                            .get_or_insert_with(|| parse_quote_spanned! { next_span => >});
+                        build_args
+                            .gens
+                            .params
+                            .push(parse_quote_spanned! { next_span => #state_type});
+                        build_args
+                            .gens
+                            .make_where_clause()
+                            .predicates
+                            .extend::<Punctuated<WherePredicate, Comma>>(
+                                parse_quote_spanned! { next_span =>
+                                    #next_type: ::server_fns::axum::extract::FromRef<#state_type>,
+                                    #state_type: ::std::marker::Send + ::std::marker::Sync
+                                }
+                            );
+
+                        build_args.args.push(parse_quote_spanned! { next_span =>
+                            #state_arg: #state_type
+                        });
+
+                        build_args
+                            .state_args
+                            .push(parse_quote_spanned! { next_span => #state_arg });
+                    }
+
+                    build_args
+                });
 
             Ok(Self {
                 span,
